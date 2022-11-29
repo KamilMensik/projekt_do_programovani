da6bb77227e2eccd83ecb9c96792d4942b96c656
Kamil Mensik <kamil.mensik@juicymo.cz>
2022-11-29 20:04:55 +0100

    Add function 1

---
+++ function1.py
@@ -0,0 +1,15 @@
+def file_load():
+    while True:
+        soubor = ''
+        file_path = input('Vlozte nazev souboru: ')
+        try:
+            soubor = open(file_path, encoding='utf-8')
+            return soubor
+        except:
+            print('Soubor nebyl nalezen.')
+
+
+def file_lines_to_list(file):
+    print(file.read().splitlines())
+
+file_lines_to_list(file_load())
